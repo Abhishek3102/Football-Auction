76af4eab3ae2efb4ecda5bef6064279648cbf054
Abhishek3102 <ankushpan694@gmail.com>
2025-12-02 17:18:02 +0530

    feat: define client API base URL configuration

--- client/lib/config.js
+++ client/lib/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://football-auction-1e1o.onrender.com" || "http://localhost:5000";
